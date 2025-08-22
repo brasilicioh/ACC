9a664279f93f1ab4cdabfc0ce7f6df4ff9433446
brasilicioh <brasiliciohcr@gmail.com>
2025-08-22 12:04:29 -0300

    mudando nome

--- site/src/LayoutNPC.jsx
+++
@@ -1,9 +0,0 @@
-export default function FichaNPC({ info, atributos, pericias }) {
-    return (
-        <>
-            <p>Nome: {info.nome}</p>
-            <p>Aparencia: {atributos.aparencia}</p>
-            <p>Destreza: {pericias.destreza}</p>
-        </>
-    )
-}
