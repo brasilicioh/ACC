13155b1a9707a178e330317fe7e24117758342db
brasilicioh <brasiliciohcr@gmail.com>
2025-09-02 12:36:44 -0300

    tirando numeros do dict de habilidades

--- core/formatarHabilidade.py
+++ core/formatarHabilidade.py
@@ -6,8 +6,6 @@ with open("habilidades.txt", "r", encoding="utf-8") as file:
     texto = file.read()
     linhas = texto.split("\n")
 
-    atributo = {"aparencia": 0, "sabedoria": 0, "tamanho": 0}
-    pericia = {"destreza": 0, "forca": 0, "intelecto": 0, "labia": 0, "percepcao": 0, "poder": 0, "precisao": 0, "psicologia": 0, "tecnica": 0, "vigor": 0, "sorte": 0}
     nomeHabilidade = ""
     descricaoHabilidade = ""
 
@@ -20,12 +18,12 @@ with open("habilidades.txt", "r", encoding="utf-8") as file:
             count += 1
         elif count % 2 == 1:
             descricaoHabilidade = linha
-            habilidades[nomeHabilidade] = [descricaoHabilidade, atributo.copy(), pericia.copy()]
+            habilidades[nomeHabilidade] = [descricaoHabilidade]
             count += 1
 
 formatado = "habilidades = {\n"
 for habilidade in habilidades:
-    formatado += f"    '{habilidade}': [\n        '{habilidades[habilidade][0]}', \n        {habilidades[habilidade][1]}, \n        {habilidades[habilidade][2]},\n    ], \n"
+    formatado += f"    '{habilidade}': [\n        '{habilidades[habilidade][0]}',\n    ], \n"
 formatado += "}"
 with open("habilidades.py", "w", encoding="utf-8") as file:
     file.write(formatado)
