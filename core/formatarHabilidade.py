ef66ef05c315bf00b244b1012ab98b2efd62bd8a
brasilicioh <brasiliciohcr@gmail.com>
2025-09-04 11:19:25 -0300

    mudando formato dos dicts de habilidades

--- core/formatarHabilidade.py
+++ core/formatarHabilidade.py
@@ -18,12 +18,12 @@ with open("habilidades.txt", "r", encoding="utf-8") as file:
             count += 1
         elif count % 2 == 1:
             descricaoHabilidade = linha
-            habilidades[nomeHabilidade] = [descricaoHabilidade]
+            habilidades[nomeHabilidade] = descricaoHabilidade
             count += 1
 
 formatado = "habilidades = {\n"
 for habilidade in habilidades:
-    formatado += f"    '{habilidade}': [\n        '{habilidades[habilidade][0]}',\n    ], \n"
+    formatado += f"    '{habilidade}': '{habilidades[habilidade]}',\n"
 formatado += "}"
 with open("habilidades.py", "w", encoding="utf-8") as file:
     file.write(formatado)
